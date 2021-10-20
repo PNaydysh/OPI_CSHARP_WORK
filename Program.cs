bfae07e38f75eda1395495596c4478c49f967bcb
PNaydysh <92798370+PNaydysh@users.noreply.github.com>
2021-10-20 12:41:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int res = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                res = res + i;
+            }
+
+            Console.WriteLine(res);
+        }
+    }
+
